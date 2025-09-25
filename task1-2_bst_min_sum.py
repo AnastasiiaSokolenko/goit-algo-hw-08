c4b15ef9e03a641a14d0f61efdfc34d852028799
Anastasiia Sokolenko <sokolenko.anastasiia@gmail.com>
2025-09-25 15:51:49 +0200

    Added task2 impl: max func, changed file name to task1-2..

---
+++ task1-2_bst_min_sum.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def max_value_node(node):
+    current = node
+    while current.right:
+        current = current.right
+    return current
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+def sum_tree(node):
+    if node is None:
+        return 0
+    return node.val + sum_tree(node.left) + sum_tree(node.right)
+
+if __name__ == "__main__":
+    root = Node(15)
+    keys = [2, 3, 1, 8, 16, 25, 24]
+
+    for key in keys:
+        root = insert(root, key)
+
+    print(root)
+    print("Мінімальний елемент:", min_value_node(root).val,  #1
+          "Максимальний елемент:", max_value_node(root).val, #25
+          "Сума елементів дерева:", sum_tree(root), "\n")  #94
+    
